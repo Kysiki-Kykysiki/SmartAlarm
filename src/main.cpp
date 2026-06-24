3e46a87ed697fc54043e9d676263936dae0860f8
SosiskinMobile <bebrik162@gmail.com>
2026-06-24 13:27:38 +0300

    Добавлен Специальный режим + Функции

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+const long max_time = 60000;
+long delalay = 0;
+long cur_time = 0;
+byte moves = 0;
+
 void setup() {
   Serial.begin(9600);
   lcd.backlight();
@@ -16,32 +21,60 @@ void setup() {
   pinMode(3, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  lcd.clear();
-  Serial.println(digitalRead(7));
+void Alert(){
   if(digitalRead(2)==HIGH){
+  lcd.setCursor(0,0);
+  lcd.print("AUTOMATIC ALERT");
+  lcd.setCursor(0,1);
+  lcd.print("Status: Alert");
+  tone(9,31);
+  digitalWrite(3,HIGH);
+  delay(10000);
+  }
+  if (digitalRead(7)==HIGH){
+  lcd.setCursor(0,0);
+  lcd.print("MANUAL ALERT");
+  lcd.setCursor(0,1);
+  lcd.print("Status: Alert");
+  digitalWrite(9,HIGH);
+  digitalWrite(3,HIGH);
+  delay(15000);
+  }
+}
+
+void Time() {
+  cur_time = millis() - delalay;
+  if(max_time - cur_time <= 0){
+    delalay = millis();
+  }else{
+    if(digitalRead(2)==HIGH){
+      moves += 1;
+    }
+    if(moves >= 5){
+    moves = 0;
     lcd.setCursor(0,0);
     lcd.print("INTRUDER ALERT");
     lcd.setCursor(0,1);
     lcd.print("Status: Alert");
     tone(9,31);
     digitalWrite(3,HIGH);
-  }else if (digitalRead(7)==HIGH){
-    lcd.setCursor(0,0);
-    lcd.print("MANUAL ALERT");
-    lcd.setCursor(0,1);
-    lcd.print("Status: Alert");
-    digitalWrite(9,HIGH);
-    digitalWrite(3,HIGH);
-    }else{
-      noTone(9);
-    lcd.setCursor(0,0);
-    lcd.print("-SmartAlarm-");
+    delay(10000);
+    }
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  lcd.clear();
+  Serial.println(digitalRead(7));
+  Alert();
+  // Time();
+  noTone(9);
+  lcd.setCursor(0,0);
+  lcd.print("-SmartAlarm-");
   lcd.setCursor(0,1);
   lcd.print("Status: Calm");
   digitalWrite(9,LOW);
   digitalWrite(3,LOW);
-  }
   delay(100);
 }
